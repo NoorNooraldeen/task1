4a224881761f0846f11121e717ef6e7621d2a309
ABK <ABK@DESKTOP-7TVO42H>
2024-12-16 22:21:56 +0300

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,65 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+//task1 primitive typs 
+// declair and assign a value 
+using System;
+using System.Runtime.InteropServices;
+
+string theSring = " noor ";
+double theDouble = 3.8;
+int theInteger = 0;
+bool theBoolean = false;
+//print each varible 
+Console.WriteLine (theSring);
+Console.WriteLine (theDouble);
+Console.WriteLine (theInteger);
+Console.WriteLine (theBoolean);
+
+//task 2 primitive typs
+
+int num = 7; // creat an integer and store a number inside it 
+double result = num; // converting the int to decimle using double 
+Console.WriteLine("result = " + result ); //print the result on the screen 
+
+//bouns part 1 
+// char is used to store a single varible 
+
+//task 2 variables 
+string studentName = " Abdullah ";// store the name in a string 
+Console.WriteLine("student name is :" + studentName); // printing 
+double gradePointAverage = 4.3;
+Console.WriteLine(" the grade point average is  :" + gradePointAverage); // printing
+string favSubject = "PE";
+Console.WriteLine(" fav subjet :" + favSubject ); // printing
+
+double width = 5.1;
+double height = 3.3;
+double area = width * height;
+Console.WriteLine(" the area is " + area);
+
+//bounse 2
+// constance is a varible that you can assigne it once but the varible you can make
+//changes befor and after executing
+
+//taske control flow 
+int currentHour = 15;
+if (currentHour >= 11 && currentHour <= 14) // check if the hour is betwen 11 or 14 
+{ 
+    Console.WriteLine("LUNCHING "); //printing 
+}
+else 
+{ 
+    Console.WriteLine("not the time for lunching"); // if the hour is not betwen 11 or 14 print this 
+}
+
+
+
+
+
+
+
+
+
+
+
